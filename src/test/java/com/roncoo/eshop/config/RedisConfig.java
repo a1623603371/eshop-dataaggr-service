92958a5c031b57b5beaeaa4607f22cf23208403e
a1623603371 <1623603371@qq.com>
2019-05-15 19:50:59 +0800

    first commit

---
+++ src/test/java/com/roncoo/eshop/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.roncoo.eshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+public class RedisConfig {
+
+    public static JedisPool jedisPool() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(100);
+        config.setMaxIdle(5);
+        config.setMaxWaitMillis(1000 * 10);
+        config.setTestOnBorrow(true);
+        return new JedisPool(config, "192.168.33.11", 6379);
+    }
+
+}
